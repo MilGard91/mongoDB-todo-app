bc438251661bce40b174c540ff0edeebc32a61f8
Miljan Milidrag <m.milidrag25@gmail.com>
2018-05-27 23:29:09 +0200

    separated db for tests and development

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
-const {app} = require('../server');
+const {app} = require('../server').default;
 const {Todo} = require('../models/todo');
 
 const todos = [{
