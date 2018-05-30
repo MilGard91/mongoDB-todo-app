f8d0571fc2e48c09fa98ddecb8b468dce75a1b39
Miljan Milidrag <m.milidrag25@gmail.com>
2018-05-30 18:16:58 +0200

    Add test POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -230,3 +230,53 @@ describe('POST /users', () => {
       .end(done)
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: user[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.header['x-auth'].toExist());
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err)
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch(e => done(e));
+      });
+  });
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'bla'
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.header['x-auth'].toNotExist());
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err)
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(e => done(e));
+      });
+  });
+});
