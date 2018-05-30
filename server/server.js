125c9408e20440c1d56cc604948667c7e99f2b48
Miljan Milidrag <m.milidrag25@gmail.com>
2018-05-30 17:54:54 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -118,6 +118,20 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send();
 });
 
+// POST /users/login {email, password}
+
+app.post('users/login' ,(req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then(user => {
+    return user.generateAuthToken().then(token => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch(e => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`)
 });
